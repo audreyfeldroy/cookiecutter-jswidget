140a0c48893ae723dccffc58cdbe5b70a308a086
Audrey Roy <audreyr@gmail.com>
2013-09-12 14:39:01 +0200

    Add widget multi-instantiation support. Fix #1.

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.repo_name}}.js
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.repo_name}}.js
@@ -37,4 +37,23 @@
 
     this.{{ cookiecutter.className }} = {{ cookiecutter.className }};
 
+    // Wrapper to allow for multiple instantiations in a single page
+    // HACK: This is similar to http://git.io/o8TOIA, but non-jQuery
+    //  and adds a property to window. I'm not 100% sure is alright to add to
+    //  the global window object. I just needed a global object to use.
+    //  There may be edge cases where this doesn't work. File an issue if you
+    //  find any problems. Better yet, submit a pull request.
+    window.{{ cookiecutter.pluginName }} = function (element, options) {
+
+      // Get the actual DOM element obj, so we can modify it
+      var {{ cookiecutter.pluginName }}_element = document.querySelector(element);
+      if ({{ cookiecutter.pluginName }}_element) {
+        if (typeof {{ cookiecutter.pluginName }}_element["plugin_" + {{ cookiecutter.pluginName }}] === "undefined") {
+          // Associate the actual DOM element obj with a new {{ cookiecutter.className }} obj
+          {{ cookiecutter.pluginName }}_element["plugin_" + {{ cookiecutter.pluginName }}] = new {{ cookiecutter.className }}(element, options);
+        }
+      }
+      return this;
+    };
+
 })( window, document );
